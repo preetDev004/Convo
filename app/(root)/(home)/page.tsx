c0372862e7e0cb29eef95e448e8bad3d855cd577
preet patel <preetpatelofficial004@gmail.com>
2024-07-25 22:40:35 +0530

    Realtime change on hero page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,12 +5,7 @@ const Home = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover bg-no-repeat">
-        <div className="flex flex-col justify-between h-full px-5 py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base">
-            Upcoming Meeting at: 12:30 PM
-          </h2>
-          <Clock/>
-        </div>
+        <Clock />
       </div>
       <MeetingTypeList />
     </section>
--- components/Clock.tsx
+++ components/Clock.tsx
@@ -1,31 +1,65 @@
-"use client"
-import React, { useEffect, useState } from 'react'
+"use client";
+import { useGetCalls } from "@/hooks/useGetCalls";
+import { useUser } from "@clerk/nextjs";
+import { Call } from "@stream-io/video-react-sdk";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const Clock = () => {
-    const [date, setDate] = useState(new Date())
-    
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setDate(new Date());
-        }, 30000);
+  const [date, setDate] = useState(new Date());
+  const [upcomingCall, setUpcomingCall] = useState<Call | undefined>();
+  const { upcomingCalls } = useGetCalls();
+  const user = useUser();
 
-        return () => clearInterval(interval);
-    }, [])
-  
-    const time = date.toLocaleTimeString("en-US", {
+  useEffect(() => {
+    setUpcomingCall(upcomingCalls[0]);
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 30000);
+
+    return () => clearInterval(interval);
+  }, [upcomingCalls]);
+
+  const callTime =
+    upcomingCall?.state?.startsAt &&
+    upcomingCall?.state?.startsAt.toLocaleTimeString("en-US", {
       hour: "2-digit",
       minute: "2-digit",
     });
-    const fmtDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-      date
-    );
+
+  const time = date.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const fmtDate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full",
+  }).format(date);
+
+  const heroStr =
+    (user?.user?.firstName?.length && user?.user?.firstName?.length > 15
+      ? user.user?.firstName?.substring(0, 15) + "..."
+      : user.user?.firstName) || "Start seemless calling with Convvo";
 
   return (
-    <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-5xl">{time}</h1>
-            <p className="text-md lg:text-lg font-medium text-sky-1">{fmtDate}</p>
-          </div>
-  )
-}
+    <div className="flex flex-col justify-between h-full px-5 py-8 lg:p-11">
+      {upcomingCalls && upcomingCalls.length > 0 ? (
+        <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base">
+          Upcoming Meeting at:{" "}
+          <Link className="hover:underline" href={"/upcoming"}>
+            {callTime}
+          </Link>
+        </h2>
+      ) : (
+        <h2 className="glassmorphism w-fit rounded p-2 text-center text-base">
+          Welcome, {heroStr}!
+        </h2>
+      )}
+      <div className="flex flex-col gap-2">
+        <h1 className="text-4xl font-extrabold lg:text-5xl">{time}</h1>
+        <p className="text-md lg:text-lg font-medium text-sky-1">{fmtDate}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Clock
+export default Clock;
